e49f528cccce6c6a7d107b8bf5830e4a3e6c862c
Jason Wang <wanghung07@gmail.com>
2022-08-01 15:13:04 +0800

    FundMe.sol updates

--- FundMe.sol
+++ FundMe.sol
@@ -11,6 +11,12 @@ contract FundMe {
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(
             msg.value.getConversionRate() >= minimumUsd,
@@ -18,5 +24,39 @@ contract FundMe {
         );
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address[](0);
+
+        /*
+        // actually withdraw the funds
+        // 1. transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        // 2. send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+        */
+
+        // 3. call
+        (bool sendSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(sendSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner");
+        _;
     }
 }
